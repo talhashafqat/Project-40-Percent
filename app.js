9ebeba29b857506b798336aacabc1282625ed897
talhashafqat <52835728+talhashafqat@users.noreply.github.com>
2020-11-20 15:10:11 +0500

    Sign Up Page Added

--- app.js
+++ app.js
@@ -13,9 +13,24 @@ app.get("/", function (req, res) {
     res.render("signin");
 });
 
+app.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
 app.post("/signin", function (req, res) {
-    console.log(req.body.email);
-    console.log(req.body.password);
+    const userEmail = req.body.userEmail;
+    const userPassword = req.body.userPassword;
+
+    console.log(userEmail);
+    console.log(userPassword);
+});
+
+app.post("/signup", function (req, res) {
+    const userName = req.body.userName;
+    const userEmail = req.body.userEmail;
+    const userPassword = req.body.userPassword;
+
+    console.log(userName + ' ' + userEmail + ' ' + userPassword);
 });
 
 app.listen(3000, function () {
